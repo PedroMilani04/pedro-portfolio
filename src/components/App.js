526355810de01875ad8fbe5a9f0569527cc9d348
Pedro Henrique Milani Vagula <pedrohenriquehmv@gmail.com>
2022-12-07 20:56:48 -0300

    PC navbar

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+
+import '../css/App.css';
+import Navbar from './Navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import "../css/Navbar.css"
+import { useState } from "react";
+
+function Navbar() {
+    const [isActive, setIsActive] = useState(false);
+
+    function handleClick() {
+        // 👇️ toggle isActive state on click
+        setIsActive(isActive => !isActive);
+      };
+
+    return (
+        <header>
+            <nav>
+                <h1> <a href="#">Pedro</a> </h1>
+                <div className="mobile-menu"  onClick={handleClick}>
+                    <div className="line1" />
+                    <div className="line2" />
+                    <div className="line3" />
+                </div>
+                <ul className={isActive ? 'nav-list-active' : 'nav-list'}>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Skills</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default Navbar;
