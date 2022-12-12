b4083fad19c071e5627972a63f2243ffe1d1ade3
Pedro Henrique Milani Vagula <pedrohenriquehmv@gmail.com>
2022-12-12 16:06:35 -0300

    about and info adjustments

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -11,11 +11,12 @@ export default function AboutMe() {
             <div className="personalinfo">
             <div className="image"></div>
             <div className="personalinfochild">
-                <div className="summary"> pedropedropedropedropedropedropedropedropedropedropedropedropedropedropedropedropedropedropedropedropedropedropedropedropedropedropedropedropedropedro </div>
+                <div className="summary"> FrontEnd Dev. Seeking for professional, technical and work environment development. Always giving my best, being in constant search for knowledge and personal improvement, sharing my current knowledge and skills, while acquiring more and more along the way.
+ </div>
                 <div className="cards">
-                    <div className="years">02 years</div>
-                    <div className="projects">9 projects</div>
-                    <div className="companies">1 company</div>
+                    <div className="years"><span className="number">02</span> Years of experience</div>
+                    <div className="projects"><span className="number">09</span> Projects made</div>
+                    <div className="companies"><span className="number">01</span> Company worked</div>
                 </div>
                 <button className="cv"><span>Download CV</span></button>
                 </div>
