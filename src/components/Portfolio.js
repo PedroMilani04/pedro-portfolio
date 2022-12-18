e81a74f93ee44f997d7b9a199a41f20e6a52a663
Pedro Henrique Milani Vagula <pedrohenriquehmv@gmail.com>
2022-12-17 22:17:32 -0300

    images correction

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,8 @@
 import "../css/Portfolio.css"
-
-import "swiper/css";
-import "swiper/css/navigation";
-
-
+import image1 from "../img/portfolio1.png"
+import image2 from "../img/portfolio2.png"
+import image3 from "../img/portfolio3.png"
+import image4 from "../img/portfolio4.png"
 
 
 export default function Portfolio() {
@@ -17,16 +16,16 @@ export default function Portfolio() {
                 <div className="coluna">
                     <div className="row1">
                         <div className="portfolio_content grid">
-                            <img src="https://data.terabox.com/thumbnail/82153b596ee7c31cdd2e85aa69b3c662?fid=4401413037865-250528-124596343550505&rt=pr&sign=FDTAER-DCb740ccc5511e5e8fedcff06b081203-bgKa86X65fdFUh9JBOFLxyfeF0w%3d&expires=8h&chkbd=0&chkv=0&dp-logid=52526896686572885&dp-callid=0&time=1671224400&size=c1920_u1080&quality=90&vuk=4401413037865&ft=image&autopolicy=1" className="portimg1"></img>
+                            <img src={image1} className="portimg1"></img>
 
                             <div className="portfolio_data">
                                 <h3 className="portfolio_title">Destination</h3>
-                                <p className="portfolio_desc">A simple "Catalog" Project using HTML5/CSS3/JS, showing various destinations around the world. Made specially for HTML5 and CSS3 training.</p>
+                                <p className="portfolio_desc">A simple "Catalog" Project using HTML5/CSS3/JS, showcasing various destinations around the world. Made specially for HTML5 and CSS3 training.</p>
                                 <button className="goto"><span>Go to project</span></button>
                             </div>
                         </div>
                         <div className="portfolio_content grid">
-                            <img src="https://data.terabox.com/thumbnail/d2b7b070911023c6120f60d7e02a7c69?fid=4401413037865-250528-1065315477914809&rt=pr&sign=FDTAER-DCb740ccc5511e5e8fedcff06b081203-k%2bOvhKjeNkviMFmz6PmWgi9rVL8%3d&expires=8h&chkbd=0&chkv=0&dp-logid=52526896686572885&dp-callid=0&time=1671224400&size=c1920_u1080&quality=90&vuk=4401413037865&ft=image&autopolicy=1" className="portimg1"></img>
+                            <img src={image2} className="portimg1"></img>
 
                             <div className="portfolio_data">
                                 <h3 className="portfolio_title">Web Pokédex</h3>
@@ -37,16 +36,16 @@ export default function Portfolio() {
                     </div>
                     <div className="row2">
                         <div className="portfolio_content grid">
-                            <img src="https://data.terabox.com/thumbnail/dc9e827f156c74961ea5fe549251cd57?fid=4401413037865-250528-1115780078137063&rt=pr&sign=FDTAER-DCb740ccc5511e5e8fedcff06b081203-lj8ckywjJW7OYmgt4xUK2y5r9fk%3d&expires=8h&chkbd=0&chkv=0&dp-logid=52526896686572885&dp-callid=0&time=1671224400&size=c1920_u1080&quality=90&vuk=4401413037865&ft=image&autopolicy=1" className="portimg1"></img>
+                            <img src={image3} className="portimg1"></img>
 
                             <div className="portfolio_data">
                                 <h3 className="portfolio_title">Tenzi Game</h3>
-                                <p className="portfolio_desc">Project made with ReactJS and CSS3, inspired by the dice game "Tenzi". The objective is to roll the dices until every number is the same.</p>
+                                <p className="portfolio_desc">Project made with ReactJS and CSS3, inspired by the dice game "Tenzi". The objective is to roll the dices until all the numbers are the same.</p>
                                 <button className="goto"><span>Go to project</span></button>
                             </div>
                         </div>
                         <div className="portfolio_content grid">
-                            <img src="https://data.terabox.com/thumbnail/c29c38b6a444f200d3032909774b2a0a?fid=4401413037865-250528-194655282546607&rt=pr&sign=FDTAER-DCb740ccc5511e5e8fedcff06b081203-dQCxiShoSsx0QU62KtA8nufVEtg%3d&expires=8h&chkbd=0&chkv=0&dp-logid=52526896686572885&dp-callid=0&time=1671224400&size=c1920_u1080&quality=90&vuk=4401413037865&ft=image&autopolicy=1" className="portimg1"></img>
+                            <img src={image4} className="portimg1"></img>
 
                             <div className="portfolio_data">
                                 <h3 className="portfolio_title">React Calculator</h3>
