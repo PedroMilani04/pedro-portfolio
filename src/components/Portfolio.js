c582f4683657424d58c5d6c8ba9f1419eb74c9fe
Pedro Henrique Milani Vagula <pedrohenriquehmv@gmail.com>
2023-01-17 22:31:27 -0300

    added weather report project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,7 +3,7 @@ import image1 from "../img/portfolio1.png"
 import image2 from "../img/portfolio2.png"
 import image3 from "../img/portfolio3.png"
 import image4 from "../img/portfolio4.png"
-
+import image5 from "../img/portfolio5.png"
 
 export default function Portfolio() {
     return (
@@ -13,45 +13,61 @@ export default function Portfolio() {
             <div className="intro">My works</div>
 
             <div className="portfolio_container container">
-                <div className="coluna">
-                    <div className="row1">
-                        <div className="portfolio_content grid">
-                            <img src={image1} className="portimg1"></img>
+                <div className="containercolunas">
+                    <div className="coluna">
+                        <div className="row1">
+                            <div className="portfolio_content grid">
+                                <img src={image1} className="portimg1"></img>
 
-                            <div className="portfolio_data">
-                                <h3 className="portfolio_title">Destination</h3>
-                                <p className="portfolio_desc">A simple "Catalog" Project using HTML5/CSS3/JS, showcasing various destinations around the world. Made specially for HTML5 and CSS3 training.</p>
-                                <a href="https://destination-viagem.vercel.app/"><button className="goto"><span>Go to project</span></button></a>
+                                <div className="portfolio_data">
+                                    <h3 className="portfolio_title">Destination</h3>
+                                    <p className="portfolio_desc">A simple "Catalog" Project using HTML5/CSS3/JS, showcasing various destinations around the world. Made specially for HTML5 and CSS3 training.</p>
+                                    <a href="https://destination-viagem.vercel.app/"><button className="goto"><span>Go to project</span></button></a>
+                                </div>
+                            </div>
+                            <div className="portfolio_content grid">
+                                <img src={image2} className="portimg1"></img>
+
+                                <div className="portfolio_data">
+                                    <h3 className="portfolio_title">Web Pokédex</h3>
+                                    <p className="portfolio_desc">A web project using HTML5, CSS3 and JavaScript to create a stylized pokédex based on the game franchise "Pokémon".</p>
+                                    <a href="https://pokedex-ph.vercel.app/"><button className="goto"><span>Go to project</span></button></a>
+                                </div>
                             </div>
                         </div>
-                        <div className="portfolio_content grid">
-                            <img src={image2} className="portimg1"></img>
+                        <div className="row2">
+                            <div className="portfolio_content grid">
+                                <img src={image3} className="portimg1"></img>
 
-                            <div className="portfolio_data">
-                                <h3 className="portfolio_title">Web Pokédex</h3>
-                                <p className="portfolio_desc">A web project using HTML5, CSS3 and JavaScript to create a stylized pokédex based on the game franchise "Pokémon".</p>
-                                <a href="https://pokedex-ph.vercel.app/"><button className="goto"><span>Go to project</span></button></a>
+                                <div className="portfolio_data">
+                                    <h3 className="portfolio_title">Tenzi Game</h3>
+                                    <p className="portfolio_desc">Project made with ReactJS and CSS3, inspired by the dice game "Tenzi". The objective is to roll the dices until all the numbers are the same.</p>
+                                    <a href="https://tenzi-game-project.vercel.app/"><button className="goto"><span>Go to project</span></button></a>
+                                </div>
+                            </div>
+                            <div className="portfolio_content grid">
+                                <img src={image4} className="portimg1"></img>
+
+                                <div className="portfolio_data">
+                                    <h3 className="portfolio_title">React Calculator</h3>
+                                    <p className="portfolio_desc">A fully functional calculator made using ReactJS, including all the basic operations and features. </p>
+                                    <a href="https://react-calculator-kappa-ten.vercel.app/" ><button className="goto"><span>Go to project</span></button></a>
+
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="row2">
-                        <div className="portfolio_content grid">
-                            <img src={image3} className="portimg1"></img>
+                    <div className="coluna2">
+                        <div className="row3">
+                            <div className="portfolio_content grid forecast">
+                                <img src={image5} className="portimg1"></img>
 
-                            <div className="portfolio_data">
-                                <h3 className="portfolio_title">Tenzi Game</h3>
-                                <p className="portfolio_desc">Project made with ReactJS and CSS3, inspired by the dice game "Tenzi". The objective is to roll the dices until all the numbers are the same.</p>
-                                <a href="https://tenzi-game-project.vercel.app/"><button className="goto"><span>Go to project</span></button></a>
-                            </div>
-                        </div>
-                        <div className="portfolio_content grid">
-                            <img src={image4} className="portimg1"></img>
+                                <div className="portfolio_data">
+                                    <h3 className="portfolio_title">Weather Forecast</h3>
+                                    <p className="portfolio_desc">A Weather Forecast application made with ReactJS and OpenWeatherMap API, containing 200.000+ cities, temperature, wind speed, humidity and more! </p>
+                                    <a href="https://react-weather-report.vercel.app/" ><button className="goto"><span>Go to project</span></button></a>
 
-                            <div className="portfolio_data">
-                                <h3 className="portfolio_title">React Calculator</h3>
-                                <p className="portfolio_desc">A fully functional calculator made using ReactJS, including all the basic operations and features. </p>
-                                <a href="https://react-calculator-kappa-ten.vercel.app/" ><button className="goto"><span>Go to project</span></button></a>
-
+                                </div>
                             </div>
                         </div>
                     </div>
